2459eef8ec42d33d76cc7fce9ba3dff860b3fbfe
Federico Perez <fperez@mobilecomputing.com.ar>
2020-08-28 19:06:19 -0300

    Se actualizan estilos globales, se agrega validacion de la page new-offer.

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-new-offer',
@@ -7,13 +9,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewOfferPage implements OnInit {
 
-  constructor() { }
+  public newOfferForm: FormGroup;
+
+  public get title(): any {
+    return this.newOfferForm.get('title');
+  }
+  public descMaxLen: number = 180;
+  public get description(): any {
+    return this.newOfferForm.get('description');
+  }
+  public get price(): any {
+    return this.newOfferForm.get('price');
+  }
+  public get dateFrom(): any {
+    return this.newOfferForm.get('dateFrom');
+  }
+  public get dateTo(): any {
+    return this.newOfferForm.get('dateTo');
+  }
+
+  constructor(
+    private navCtrl: NavController) { }
 
   ngOnInit() {
+    this.createForm();
+  }
+
+  public createForm() {
+    this.newOfferForm = new FormGroup({
+      title: new FormControl(null, {
+        updateOn: 'change',
+        validators: [Validators.required]
+      }),
+      description: new FormControl(null, {
+        updateOn: 'change',
+        validators: [Validators.required, Validators.maxLength(this.descMaxLen)]
+      }),
+      price: new FormControl(null, {
+        updateOn: 'change',
+        validators: [Validators.required, Validators.min(1)]
+      }),
+      dateFrom: new FormControl(null, {
+        updateOn: 'change',
+        validators: [Validators.required]
+      }),
+      dateTo: new FormControl(null, {
+        updateOn: 'change',
+        validators: [Validators.required]
+      }),
+    });
   }
 
   public onCreateOffer() {
-    console.log('Offer created');
+    console.log(this.newOfferForm);
+    this.navCtrl.navigateBack('/places/tabs/offers');
   }
 
 }
